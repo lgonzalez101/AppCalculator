318291703dbc26f25f3a58f1bea1b26c82a3e5db
LuisGonzalez <luis.rigoma@gmail.com>
2017-06-17 11:10:35 -0600

    Se agrego el proyecto

---
+++ AppWebApi/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using AppWebApi.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var _client = new RestClient { BaseUrl = new Uri("http://localhost:61554/") };
+            var request = new RestRequest("api/values", Method.GET) { RequestFormat = DataFormat.Json };
+            var response = _client.Execute<ResultadoViewModel>(request);
+            if (response.Data == null)
+                throw new Exception(response.ErrorMessage);          
+            return View(response.Data);
+        }
+
+        [HttpGet]
+        public ActionResult Calcular(ResultadoViewModel model)
+        {
+            var  _client = new RestClient { BaseUrl = new Uri("http://localhost:61554/") };
+            var request = new RestRequest("api/values", Method.POST) { RequestFormat = DataFormat.Json };
+            request.AddBody(model);
+            var response = _client.Execute<ResultadoViewModel>(request);
+            if (response.StatusCode != HttpStatusCode.Created)
+            {
+                return Json(response.Data, JsonRequestBehavior.AllowGet);
+            }
+                throw new Exception(response.ErrorMessage);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AppWebApi/Models/ResultadoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebApi.Models
+{
+    public class ResultadoViewModel
+    {
+        public string Resultado { get; set; }
+    }
+}
